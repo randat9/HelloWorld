8b5ca7766e0528a182bbc55d2edb66134b8991c7
Adrian <a.oles1337@gmail.com>
2018-02-27 20:49:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mn/helloworld/Hello_World.kt
@@ -0,0 +1,12 @@
+package com.example.mn.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Hello_World : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello__world)
+    }
+}
